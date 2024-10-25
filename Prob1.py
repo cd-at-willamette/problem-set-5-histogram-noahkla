4e0b3e134dd86375691a386305e9ec41b448fa29
noahkla <noahklarreich@gmail.com>
2024-10-25 10:37:53 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,28 +1,33 @@
 from pgl import *
 
 #1a
-def create_histogram_array(data:list[int])->list[int]:
-    pass
+def create_histogram_array(data):
+    h = [0]*10
+    for d in data:
+        h[d] += 1
+    return h
 
 #1b
-def print_histogram(hist:list[int]) -> None:
-    pass
+def print_histogram(hist):
+    for i in range(len(hist)):
+        print(str(i) + ': ' + hist[i]*'*')
 
 #1c
-def graph_histogram(hist:list[int], width:int, height:int) -> None:
-
+def graph_histogram(hist, width, height) -> None:
+    gw = GWindow(width, height)
     def my_rect(x,y,w,h,color):
         rect = GRect(x,y,w,h)
         rect.set_filled(True)
         rect.set_color(color)
         gw.add(rect)
-
-    pass
+    w = width/10
+    u = 400/max(hist)
+    for i in range(10):
+        my_rect(i*w, height-hist[i]*u, w, hist[i]*u, 'Red')
 
 # Some testing printouts for your use!
 PI_DIGITS = [3,1,4,1,5,9,2,6,5,3,5,5,8,9,7,9]
 hist = create_histogram_array(PI_DIGITS)
-print(hist)
-#print_histogram(hist) # uncomment to test
-#graph_histogram(hist, 400, 400) # uncomment to test
+print_histogram(hist) # uncomment to test
+graph_histogram(hist, 400, 400) # uncomment to test
 
